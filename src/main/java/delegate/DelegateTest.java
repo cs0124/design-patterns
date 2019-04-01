1c561d78554bb9e7998e5de1c98cd911c0fa96f5
cs0124 <chis666@163.com>
2019-04-01 19:56:27 +0800

    update

--- src/main/java/delegate/DelegateTest.java
+++ src/main/java/delegate/DelegateTest.java
@@ -1,9 +1,15 @@
 package delegate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DelegateTest {
     public static void main(String[] args) {
 
         new Boss().command("加密",new Leader());
+
+        List<String> list = new ArrayList<String>();
+        list.add("a");
     }
 
 }
